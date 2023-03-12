a511cfbd94433196b5df7a0961100a172778a97e
Menar Selamet <menarselamet007@gmail.com>
2023-03-12 16:52:30 +0300

    another attempt at sending data failed!

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,23 @@
 import FormToDo from "./components/FormToDo";
+import ToDoList from './components/ToDoList'
 import axios from "axios";
+import { useState } from 'react';
 
 function App() {
+  const [todo, setToDo ] = useState([])
 
- const addTask = async (task) => {
+ const addTask = async (tasks) => {
     const response = await axios.post("http://localhost:3001/tasks", {
-      task,
+      tasks,
     });
-    console.log(response)
+    const updateToDo = [...todo , response.data];
+    setToDo(updateToDo);
  }
 
   return (
     <div>
       <FormToDo addTask={addTask}/>
+      <ToDoList toDo = {todo}/>
     </div>
   );
 }
---
+++ demo/src/components/ToDoList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ToDoList({ todo }) {
+
+  return <div>{todo}</div>;
+}
+
+export default ToDoList;
