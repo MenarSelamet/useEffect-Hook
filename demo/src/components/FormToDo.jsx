1e1569005178c40fbaccb80b1bd81ef60db9a8a6
Menar Selamet <menarselamet007@gmail.com>
2023-03-12 19:10:02 +0300

    All set

--- demo/src/components/FormToDo.jsx
+++ demo/src/components/FormToDo.jsx
@@ -17,7 +17,6 @@ function FormToDo({ addTask }) {
     <div className="container">
       <h2 className="addTask"> Add a Task </h2>
       <form className="form" onSubmit={handleSubmit}>
-        <label className="task">Task:</label>
         <input className="input" value={task} onChange={handleChange} />
         <button className="btn">Add!</button>
       </form>
